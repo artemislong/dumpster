30e020fe2c66dc3670f073d124d11119d70a2c01
artemislong <60078396+artemislong@users.noreply.github.com>
2023-11-01 23:08:12 -0400

    Update quiz7real.py
    rwewerwerwer

--- quiz7real.py
+++ quiz7real.py
@@ -23,47 +23,48 @@ if __name__ == "__main__":
     # get last days
     # Calculate moving averages and filter out windows with insufficient data
     goog10Day = googPrice.window(10, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 10).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
-    goog40Day = googPrice.window(20, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 20).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
+    goog40Day = googPrice.window(40, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 40).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
     msft10Day = msftPrice.window(10, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 10).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
-    msft40Day = msftPrice.window(20, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 20).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
-
-    # goog10Day.map(lambda x: "Avg GOOG 10days: " + str(x)).pprint()
-    # goog40Day.map(lambda x: "Avg GOOG 40days: " + str(x)).pprint()
-    # msft10Day.map(lambda x: "Avg MSFT 10days: " + str(x)).pprint()
-    # msft40Day.map(lambda x: "Avg MSFT 40days: " + str(x)).pprint()
+    msft40Day = msftPrice.window(40, 1).map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (max(x[0], y[0]), x[1]+y[1], x[2]+y[2])).filter(lambda xy: xy[2] == 40).map(lambda xy: (xy[0], str(xy[1] / xy[2])))
     
     # Q3
-    # different logic
-    googAvg10DayLast2 = goog10Day.window(2, 1)
-    googAvg40DayLast2 = goog40Day.window(2, 1)
-    
-    googAvg2 = googAvg10DayLast2.join(googAvg40DayLast2) # (date, (avg10, avg40))
-    # googAvg2.pprint()
     def timeDiff(x,y): 
         xs = list(map(int, x.split("-")))
-        print(xs)
         ys = list(map(int, y.split("-")))
         diff = date(xs[0],xs[1],xs[2]) - date(ys[0],ys[1],ys[2])
         return diff.days/abs(diff.days)
-    # googAvg2.map(lambda x: x[1]).pprint()
-    # schema: date, date order (negative means first row is older), difference between avg10 and 40 first row and second row
-    googAvg2Reduced = googAvg2.reduce(lambda x,y: (max(x[0],y[0]), timeDiff(x[0],y[0]), float(x[1][0]) - float(x[1][1])))
-    # googAvg2Reduced = googAvg2.reduce(lambda x,y: (max(x[0],y[0]), timeDiff(x[0],y[0]), float(x[1][0]) - float(x[1][1]), float(y[1][0]) - float(y[1][1])))
-
-    googAvg2Reduced.pprint()
-    # transforming so the first entry is newer
-    # first map: sorting differences, if x[1] is positive, x[2] is today's avg10/40 difference | 2nd map: sell if today's difference is positive, and previous wasn't
+    # cross detection and advice: 1 is buy, 2 is sell, 0 is wait
     def decide(x):
-        return 1 if (x[1] > 0 & x[2] < 0) else 2 if (x[1] < 0 & x[2] > 0) else 0
-    # googAvgDecision = googAvg2Reduced.map(lambda x: (x[0], x[2] if int(x[1]) > 0 else x[3], x[3] if int(x[1]) > 0 else x[2]))
-    # googAvgDecision.pprint()
-    # .map(lambda x: (x[0], decide(x)))
-    # googAvg2Reduced.map(lambda x: str(x[0]) + " buy " if x[1] == 1 else " sell " if x[1] == 2 else " wait " + "GOOG").pprint()                                      
-    # msftAvg10DayLast2 = msft10Day.window(2, 1)
-    # msftAvg40DayLast2 = msft40Day.window(2, 1)
-    # msftAvg2 = msftAvg10DayLast2.join(msftAvg40DayLast2)
-    # msftAvg2.pprint()
+        return 1 if ((x[1] > 0.0) & (x[2] < 0.0)) else 2 if ((x[1] < 0.0) & (x[2] > 0.0)) else 0
 
+    # GOOG
+    googAvg10DayLast2 = goog10Day.window(2, 1)
+    googAvg40DayLast2 = goog40Day.window(2, 1)
+    googAvg2 = googAvg10DayLast2.join(googAvg40DayLast2) # (date, (avg10, avg40))
+    # schema: date, date order (negative means first row is older), difference between avg10 and 40 first row and second row
+    googAvg2Reduced = googAvg2.map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (x[2]+y[2], max(x[0],y[0]), timeDiff(x[0],y[0]), float(x[1][0]) - float(x[1][1]), float(y[1][0]) - float(y[1][1]))).filter(lambda xy: xy[0] == 2)
+    # sorting
+    googAvgDecision = googAvg2Reduced.map(lambda x: (x[1], x[3] if int(x[2]) > 0 else x[4], x[4] if int(x[2]) < 0 else x[3]))
+    googResult = googAvgDecision.map(lambda x: (x[0], decide(x)))
+    goog = googResult.map(lambda x: (x[0], " buy " if x[1] == 1 else " sell " if x[1] == 2 else " wait ", "GOOG")).map(lambda x: x[0]+x[1]+x[2])
+    goog.pprint()
+    # MSFT 
+    msftAvg10DayLast2 = msft10Day.window(2, 1)
+    msftAvg40DayLast2 = msft40Day.window(2, 1)
+    msftAvg2 = msftAvg10DayLast2.join(msftAvg40DayLast2) # (date, (avg10, avg40))
+    # schema: date, date order (negative means first row is older), difference between avg10 and 40 first row and second row
+    msftAvg2Reduced = msftAvg2.map(lambda x: (x[0],x[1],1)).reduce(lambda x,y: (x[2]+y[2], max(x[0],y[0]), timeDiff(x[0],y[0]), float(x[1][0]) - float(x[1][1]), float(y[1][0]) - float(y[1][1]))).filter(lambda xy: xy[0] == 2)
+    # sorting
+    msftAvgDecision = msftAvg2Reduced.map(lambda x: (x[1], x[3] if int(x[2]) > 0 else x[4], x[4] if int(x[2]) < 0 else x[3]))
+    msftResult = msftAvgDecision.map(lambda x: (x[0], decide(x)))
+    msft = msftResult.map(lambda x: (x[0], " buy " if x[1] == 1 else " sell " if x[1] == 2 else " wait ", "MSFT")).map(lambda x: x[0]+x[1]+x[2])
+    msft.pprint()
+    def write_to_text_file(rdd):
+        with open("stock_decision.txt", "a") as file:  # Open the file in "append" mode
+            for line in rdd.collect():
+                file.write(line + "\n")
+    goog.foreachRDD(write_to_text_file)
+    msft.foreachRDD(write_to_text_file)
     
     ssc.start()
     ssc.awaitTermination()
